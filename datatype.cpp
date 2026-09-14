9cb397acd190260c9d5be1fa836015fa5a44b69c
Abhitosh Baranwal <abhitoshyt@gmail.com>
2026-09-14 16:26:44 +0530

    data types

---
+++ datatype.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    a = 3;
+    b = 4;
+    //number se start nhi ho skta var but can be started with _
+    cout<<a<<endl; //endl new line add krega
+    cout<<b; 
+    return 0;
+}
