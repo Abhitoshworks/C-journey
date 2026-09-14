62fa581ff80b2edc9a04ad08adc346c31f796483
Abhitosh Baranwal <abhitoshyt@gmail.com>
2026-09-14 21:17:57 +0530

    to take input from user

---
+++ input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter your first number :";
+    cin>>a;
+    cout<<"Enter your second number :";
+    cin>>b;
+    cout<< "a is: " <<a<<endl;
+    cout<<"b is: "<<b;
+    int c = a+b;
+    cout<<"the sum is : "<<c;
+    return 0;
+}
