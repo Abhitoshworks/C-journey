359cc52e433ecdd5e7d37103e77a079a6243d39b
Abhitosh Baranwal <abhitoshyt@gmail.com>
2026-09-15 00:11:04 +0530

    Functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
+int x(int a, int b){
+    int c;
+    c = a+b;
+    return c;
+}
 int main(){
-    cout<<"hello world"; 
+    int z = 2;
+    int s = 3;
+    cout<< x(z,s); 
     return 0;
 }
