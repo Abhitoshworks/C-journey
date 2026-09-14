50827d1d6e568298195ae8ccfe2d95a82d20babd
Abhitosh Baranwal <abhitoshyt@gmail.com>
2026-09-14 22:04:41 +0530

    switch statement

---
+++ switch.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+cout<<"Enter te value of a: ";
+cin>>a;
+switch (a)
+{
+case 18:
+    cout<<"you are 18 you can drive";
+    break;
+case 50:
+   cout<<"you are 50 you can drug";
+   break;
+default:
+   cout<<"no racism";
+    break;
+}
+ return 0; 
+}
