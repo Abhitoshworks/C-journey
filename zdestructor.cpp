900eb56a17ccfee459591b691d8ec0a0c71aa8a5
Abhitosh Baranwal <abhitoshyt@gmail.com>
2026-09-18 15:06:03 +0530

    Destructor with pointers

--- zdestructor.cpp
+++ zdestructor.cpp
@@ -1,27 +1,36 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class lh6{
-    public:
+class lh6
+{
+public:
     string name;
     int rollno;
-    int marks;
-    lh6(){
-        cout<<"Constructor called"<<endl;
+    int *marks;
+    lh6()
+    {
+        cout << "Constructor called" << endl;
         name = "Abhitosh";
-        marks = 67;
+
         rollno = 8725203;
+        marks = new int;
+        *marks = 67;
     }
-    void displaystudent(){
-        cout<<"the name of the student is "<<name<<endl;
-        cout<<"the marks of the student are "<<marks<<endl;
-        cout<<"the roll number of the student is "<<rollno<<endl;
+    void displaystudent()
+    {
+        cout << "the name of the student is " << name << endl;
+        cout << "the marks of the student are " << marks << endl;
+        cout << "the roll number of the student is " << rollno << endl;
+        cout << "the marks of the student are " << *marks << endl;
     }
-    ~lh6(){
-    cout<<"destructor called"<<endl; 
- }
+    ~lh6()
+    {
+        cout << "destructor called" << endl;
+        delete marks;
+    }
 };
- 
-int main(){
+
+int main()
+{
     lh6 random;
     random.displaystudent();
     return 0;
